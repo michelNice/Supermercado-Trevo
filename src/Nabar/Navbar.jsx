a3f582a416e605e9a0c84c2f73fd7e4ab708c514
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-08-13 22:30:15 -0300

    update

--- src/Nabar/Navbar.jsx
+++ src/Nabar/Navbar.jsx
@@ -1,25 +1,60 @@
+function Navbar() {
+  return (
+    <header>
+      
+      <div className="promo__bar">
+        <span>
+          Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10
+        </span>
+        <span className="right">
+          Receba em casa ou retire na loja
+        </span>
+      </div>
 
+      
+      <div className="main__header">
+        {/* Logo */}
+        <div className="logo">
+          <img src="/logo.png" alt="Arco-Mix Logo" />
+        </div>
 
-function Navbar (){
+        
+        <div className="search__bar">
+          <input type="text" placeholder="O que você precisa?" />
+          <button>🔍</button>
+        </div>
 
-    return (
-        <>
-            <div>
-                <div className="logo"></div>
-            </div>
+       
+        <div className="header__actions">
+          <div className="store__info">
+            <span>Retirar na loja:</span>
+            <strong>Avenida Zequinha Barreto...</strong>
+          </div>
 
-            <div>
-                 <input type="text" className="text" />
-            </div>
-
-            <div>
-                logo
-            </div>
-        </>
-
-    )
-
+          <div className="login">
+            <span>
+              Olá, faça seu login <strong>ou cadastre-se</strong>
+            </span>
+          </div>
+          <div className="cart">🛒</div>
+        </div>
+      </div>
+      <nav className="main__nav">
+        <ul>
+          <li>
+            <strong>Departamentos</strong> ⌄
+          </li>
+          <li>Mais Vendidos</li>
+          <li>Ofertas</li>
+          <li>Combos</li>
+          <li>Coleções</li>
+          <li>Dicas e Receitas</li>
+          <li>Faça seu Cartão</li>
+          <li>Acesse o App</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
