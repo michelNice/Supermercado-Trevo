9ff805b19f566b38db109c0d22a84fa48899d9fa
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-09-10 08:15:11 -0300

    update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,41 +4,48 @@ import { FaSearch, FaUser, FaShoppingCart, FaStore } from "react-icons/fa";
 
 function Navbar() {
 
-  return(
-    <header className="header">
-        <div className="promo__bar">
-         <div className="promo__content">
-          <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
-          <span>Receba em casa ou retire na loja</span>
-          <span>Sua primeira compra com desconto</span>
-        </div>
+  return (
+  <header className="header">
+    <div className="promo__bar">
+      <div className="promo__track">
+        <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
+        <span>Receba em casa ou retire na loja</span>
+        <span>Sua primeira compra com desconto</span>
+        <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
+        <span>Receba em casa ou retire na loja</span>
+        <span>Sua primeira compra com desconto</span>
       </div>
+    </div>
 
-     <div className="navbar">
+    <div className="navbar">
+      <div className="logo">logo</div>
 
-      <div className="logo">
-            logo
-      </div>
-           
-           <div className="seach__box">
-             <input type="text" placeholder="O que você precisa?" />
-             <FaSearch className="icon" />
+      <div className="seach__box">
+        <input type="text" placeholder="O que você precisa?" />
+        <FaSearch className="icon" />
       </div>
 
       <div className="actions">
-          <div className="store">
-            <FaStore /> <span>Retirar na loja:<br/>Rua Eliza Cabral de Souza, 78</span>
-          </div>
-          <div className="login">
-            <FaUser /> <span>Olá, faça seu login<br/>ou cadastre-se</span>
-          </div>
-          <div className="cart">
-            <FaShoppingCart />
-          </div>
+        <div className="store">
+          <FaStore />{" "}
+          <span>
+            Retirar na loja:<br />Rua Eliza Cabral de Souza, 78
+          </span>
         </div>
-     </div>
-    </header>
-  );
+        <div className="login">
+          <FaUser />{" "}
+          <span>
+            Olá, faça seu login<br />ou cadastre-se
+          </span>
+        </div>
+        <div className="cart">
+          <FaShoppingCart />
+        </div>
+      </div>
+    </div>
+  </header>
+);
+
 }
 
 
