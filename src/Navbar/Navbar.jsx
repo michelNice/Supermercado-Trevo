150cb9e0d49ecc10294db0ac1bb6d08688f8c0c4
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-09-17 08:21:46 -0300

    update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,52 +1,56 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 import { FaSearch, FaUser, FaShoppingCart, FaStore } from "react-icons/fa";
 
 function Navbar() {
-
   return (
-  <header className="header">
-    <div className="promo__bar">
-      <div className="promo__track">
-        <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
-        <span>Receba em casa ou retire na loja</span>
-        <span>Sua primeira compra com desconto</span>
-        <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
-        <span>Receba em casa ou retire na loja</span>
-        <span>Sua primeira compra com desconto</span>
-      </div>
-    </div>
-
-    <div className="navbar">
-      <div className="logo">logo</div>
-
-      <div className="seach__box">
-        <input type="text" placeholder="O que você precisa?" />
-        <FaSearch className="icon" />
-      </div>
-
-      <div className="actions">
-        <div className="store">
-          <FaStore />{" "}
-          <span>
-            Retirar na loja:<br />Rua Eliza Cabral de Souza, 78
-          </span>
-        </div>
-        <div className="login">
-          <FaUser />{" "}
-          <span>
-            Olá, faça seu login<br />ou cadastre-se
-          </span>
-        </div>
-        <div className="cart">
-          <FaShoppingCart />
+    <header className="header">
+      {/* Barra promocional */}
+      <div className="promo__bar">
+        <div className="promo__track">
+          <span>Compre acima de R$199 e ganhe R$10 com cupom PRIMEIRACOMPRA10</span>
+          <span>Receba em casa ou retire na loja</span>
+          <span>Sua primeira compra com desconto</span>
         </div>
       </div>
-    </div>
-  </header>
-);
 
+      {/* Navbar principal */}
+      <div className="navbar">
+        {/* Logo */}
+        <div className="logo">Logo</div>
+
+        {/* Caixa de pesquisa */}
+        <div className="search__box">
+          <input type="text" placeholder="O que você precisa?" />
+          <FaSearch className="icon" />
+        </div>
+
+        {/* Ações */}
+        <div className="actions">
+          <div className="store">
+            <FaStore className="icon" />
+            <span>
+              Retirar na loja: <br />
+              Rua Eliza Cabral de Souza
+            </span>
+          </div>
+
+          <div className="store">
+            <FaUser className="icon" />
+            <span>
+              Olá, faça seu login<br />
+              ou cadastra-se
+            </span>
+          </div>
+
+          <div className="cart">
+            <FaShoppingCart className="icon" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-
 export default Navbar;
+
