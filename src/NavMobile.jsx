908783035b33cf59df090914134f495f2eff9afa
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-10-20 07:56:56 -0300

    update

---
+++ src/NavMobile.jsx
@@ -0,0 +1,15 @@
+import React, { useState }  from "react";
+import {
+    FaBars,
+    Fatimes,
+    FaHome,
+    FaThLarge,
+    FaGift
+} from 'react-icons/fa'
+
+function NavMobile(){
+
+    const [menuOpen,setMenuOpen] = useState(false)
+}
+
+export default NavMobile
