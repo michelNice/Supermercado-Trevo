7bf86e80381f4e82c8f4728f25a9e37dc6bb3fa3
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-10-28 21:10:13 -0300

    update

--- src/Subscription/Subscription.jsx
+++ src/Subscription/Subscription.jsx
@@ -3,33 +3,41 @@ function Subscription (){
 
     return(
 
-        <>
-            <section className="sub__login">
-                    <form action="#" method="post">
-                        <div className="sub__text">
-                            <h2>Seja bem-vindo(a)!</h2>
-                            <p>Insira seus dados nos campos abaixo para fazer login</p> 
-                        </div>
-                         <div className="input__box">
-                            <input type="text" name="" id="user" />
-                            <label for="user">Email, CPF ou CNPJ*</label>
-                         </div>
-                         <div className="input__box">
-                              <input type="text" name="" id="" />
-                         </div>
-                        
-                        <a href="#">Esqueceu sua senha</a>
+     <>
+  <section className="sub__login">
+    <form action="#" method="post">
+      <div className="sub__text">
+        <h2>Seja bem-vindo(a)!</h2>
+        <p>Insira seus dados nos campos abaixo para fazer login</p>
+      </div>
 
-                        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+      <div className="input__box">
+        <input type="text" id="user" placeholder=" " required />
+        <label htmlFor="user">Email, CPF ou CNPJ*</label>
+      </div>
 
-                        <button type="submit">Entrar</button>
+      <div className="input__box">
+        <input type="password" id="password" placeholder=" " required />
+        <label htmlFor="password">Senha*</label>
+      </div>
 
-                        <p href="#">Ainda não tem uma conta?</p>
+      <a href="#">Esqueceu sua senha?</a>
 
-                        <button>Criar uma conta</button>
-                    </form>
-            </section>
-        </>
+     <script
+  src="https://www.google.com/recaptcha/api.js"
+  async
+  defer
+></script>
+
+      <button type="submit">Entrar</button>
+
+      <p>Ainda não tem uma conta?</p>
+
+      <button type="button">Criar uma conta</button>
+    </form>
+  </section>
+</>
+
     )
 
 }
