5128bab7f1458835fd94ca5c8ac755f97970cdf1
Michel Gouveia <michel-gouveia1997@hotmail.com>
2025-12-05 23:16:31 -0300

    ipdate

--- src/DepartmentsMobile/DepartmentsMobile.jsx
+++ src/DepartmentsMobile/DepartmentsMobile.jsx
@@ -1,65 +1,31 @@
-/*import './DepartmentMobile.css'
-import {
-  Splay,
-  HeartPulse,
-  Apple,
-  Wine,
-  Sandwich,
-  ShoppingBasket,
-  Fish,
-  Snowflake
-} from 'lucide-react';
+//import './DepartmentMobile.css'
 
-const  departaments = [
-
+const departaments = [
     {
-        name:'Limpeza',
-        icon: <Splay />
+        name:'hi there'
     },
     {
-        name:'Saúde e bem-estar',
-        icon:<HeartPulse />
-    },
-    {
-        name:"Hortifruti",
-        icon:<Apple />
-    }
-    ,
-    {
-       name:"Bebidas",
-       icon:<Wine />
-    },
-    {
-      name:"Salgados",
-      icon: <Sandwich />
-    },
-    {
-        name:"Mercearia",
-        icon:<ShoppingBasket />
-
-    },
-    {
-        name:"Aves, carnes e pescados",
-        icon: <Fish />
-    },
-    { name: "Resfriados e congelados",
-         icon: <Snowflake />
+        name:'it working'
     }
 ]
+
 function DepartmentMobile(){
 
     return(
         <>
-          <ul>
-              {departaments.map((item,index)=> {
-                  <li key={index}>{item.icon}</li>
-              })}
-          </ul>
+            <ul>
+            {departaments.map((item,index)=>{
+                return(
+                    <li key={index}>
+                    {item.name}
+                    </li>
+                )
+            })}
+
+            </ul>
         </>
     );
 
 }
 
 export default DepartmentMobile;
-
-*/
