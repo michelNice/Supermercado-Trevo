d48d97a47ac83a86951d4fdbb90a8b5067e913b4
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-01-09 08:23:53 -0300

    update

--- src/Departments/Departments.jsx
+++ src/Departments/Departments.jsx
@@ -1,7 +1,9 @@
-import imgDepartments5  from './imgs/img1.png'
-import imgDepartments2  from './imgs/img2.png'
-import imgDepartments3  from './imgs/img3.png'
-import imgDepartments4  from './imgs/img4.png'
+import imgDepartments1  from './imgs/dec1.png'
+import imgDepartments2  from './imgs/dec2.png'
+import imgDepartments3  from './imgs/dec3.png'
+import imgDepartments4  from './imgs/dec4.png'
+import imgDepartments5  from './imgs/dec5.png'
+import imgDepartments6  from './imgs/dec6.png'
 import "./css/Deparments.css";
 
 
@@ -11,10 +13,12 @@ function Departments(){
 
         <>
             <div className='imgs'>
-                <img src={imgDepartments5} alt="" />
+                <img src={imgDepartments1} alt="" />
                 <img src={imgDepartments2} alt="" />
                 <img src={imgDepartments3} alt="" />
                 <img src={imgDepartments4} alt="" />
+                <img src={imgDepartments5} alt="" />
+                <img src={imgDepartments2} alt="" />
             </div>
             
         </>
