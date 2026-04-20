4dbb36044edf5276f05e6d73304ec0b208a98845
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-04-20 19:19:42 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import Navbar from './Navbar/Navbar'
+import HeroSlider from './HeroSlider/HeroSlider'
 
 function App() {
   return (
-    <Navbar/>
+    <>
+     <Navbar/>
+
+     <HeroSlider />
+    </>
+   
   )
 }
 
---
+++ src/src/vite-env.d.ts
@@ -0,0 +1,26 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.css";
+
+
+declare module "swiper/css";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
