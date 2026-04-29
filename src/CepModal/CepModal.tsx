157b92610a7b16f49fbfdf7024b113468c504d4f
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-04-29 09:37:54 -0300

    update

--- src/CepModal/CepModal.tsx
+++ src/CepModal/CepModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import './CepModal.scss'
+import  {formatCep,isCepValid} from './CepModalUtils'
 
 type CepModalProps = {
     show:boolean;
@@ -17,40 +18,65 @@ const CepModal: React.FC<CepModalProps> = ({
 }) => {
   
     const [cepError,setCepError] = useState('')
+     //if (!show) return null;
 
+    const handleCepChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+        const value = formatCep(e.target.value)
+          setCep(value)
+          setCepError("")
+    }
+    const handleSubmit = ()=>{
+        if(!isCepValid(cep)){
+            setCepError("CEP inválido");
+            return;
+        }
+        setCepError("")
+        onSubmit()
+    }
     return(
-        <section className="modal__overlay">
-            <div className="modal__wrapper">
-                <div className="modal modal_trans">
-                     <button className="modal__close">×</button>
-                       <h2 className="modal__title">Qual o seu CEP?</h2>
-                       <p className="modal_descripition">
-                             Precisamos validar seu CEP para saber se o nosso serviço atende a sua região.
-                       </p>
-                       <div className="modal__input">
-                            <input 
-                            className={`modal__input ${cepError ? "input--error" : ""}`}
-                            type="text"
-                            placeholder=""
-                            maxLength={9}
-                            />
-                            <label htmlFor="" className={`$modal__label ${cepError ? "label--error" : ""}`}>
-                                Digite seu CEP*
-                            </label>
-                       </div>
-                       <div className="error__space">
-                           {cepError && <p className="error__text">{cepError}</p>}
-                       </div>
+       <section className="modal__overlay" onClick={onClose}>
+      <div className="modal__wrapper" onClick={(e) => e.stopPropagation()}>
+        <div className="modal modal__trans">
 
-                       <div className="modal__footer">
-                         <button className="modal__button">
-                            Verificar Disponibilidade
-                         </button>
-                       </div>
-                </div>
-            </div>
-        </section>
+          <button className="modal__close" onClick={onClose}>×</button>
+
+          <h2 className="modal__title">Qual o seu CEP?</h2>
+
+          <p className="modal__description">
+            Precisamos validar seu CEP para saber se o nosso serviço atende a sua região.
+          </p>
+
+          <div className="modal__inputBox">
+            <input
+              className={`modal__input ${cepError ? "input--error" : ""}`}
+              type="text"
+              placeholder=" "
+              value={cep}
+              onChange={handleCepChange}
+              maxLength={9}
+              required
+            />
+
+            <label
+              className={`modal__label ${cepError ? "label--error" : ""}`}
+            >
+              Digite seu CEP*
+            </label>
+          </div>
+
+          <div className="error__space">
+            {cepError && <p className="error__text">{cepError}</p>}
+          </div>
+
+          <div className="modal__footer">
+            <button className="modal__button" onClick={handleSubmit}>
+              Verificar Disponibilidade
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </section>
     )
 }
-
 export default CepModal;
