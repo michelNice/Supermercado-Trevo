b90c96d3fae2f7d7a9c0f238551ef76b32cd0795
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-05-15 21:50:13 -0300

    u[dste

--- src/Products/productSwiper.tsx
+++ src/Products/productSwiper.tsx
@@ -2,77 +2,55 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-
+import Product from "./Products";
+import { FiList } from "react-icons/fi";
 type ProductSectionProps = {
         setScreen: React.Dispatch<React.SetStateAction<string>>
         setShowModal: React.Dispatch<React.SetStateAction<boolean>>
         showModal: boolean
         showUnavailable: React.Dispatch<React.SetStateAction<boolean>>
         setShowUnavailable: React.Dispatch<React.SetStateAction<boolean>>
+    
 }
 
-const ProductSwiper: React.FC<ProductSectionProps> = () => {
+const ProductSwiper: React.FC<ProductSectionProps> = ({setShowModal,showModal,products,setScreen}) => {
 return (
- <>
-      <div className="products-container">
-                    <div className="hr"></div>
-                    <h2>CORRE QUE É SÓ HOJE ✨</h2>
-                    <Swiper
-                        modules={[Navigation]}
-                        spaceBetween={16}
-                        slidesPerView={2.2}
-                        navigation
-                        grabCursor={true}
-                        breakpoints={{
-                        640: { slidesPerView: 2.2 },
-                        768: { slidesPerView: 3 },
-                        1024: { slidesPerView: 5 },
-                        }}
-                        className="products-swiper"
-                    >
-                    {mixedProducts.map((product) => {
-                        return (
-                            <SwiperSlide key={product.id}>
-                                <div className="product-card">
-                                <div
-                                    className={`offer ${
-                                        product?.offer?.trim().toLowerCase() === 'exclusivo'
-                                        ? 'offer__colorDark'
-                                        : 'offer__colorLight'
-                                    }`}
-                                    >
-                                    {product?.offer}
-                                </div>
-                                    <div className="filter-icon" onClick={()=> setShowModal(true)}> <FiList /></div>
-                                    <div className="product-img">
-                                         <img src={product.image_url} alt={product.name} />
+    <>
+         <div className="product-card">
+                                    <div
+                                        className={`offer ${
+                                            product?.offer?.trim().toLowerCase() === 'exclusivo'
+                                            ? 'offer__colorDark'
+                                            : 'offer__colorLight'
+                                        }`}
+                                        >
+                                        {product?.offer}
                                     </div>
-                                    <button
-                                        className="add-btn"
-                                        onClick={()=> setScreen('login')}
-                                    >
-                                        +
-                                    </button>
-                                    <div className="product-info">
-                                        <p className="name">{product.name}</p>
-    
-                                        <div className="price">
-                                        <span className="current">R$ {product.price}</span>
-                                        <span className="unit">/{product.unit_type}</span>
+                                        <div className="filter-icon" onClick={()=> setShowModal(true)}> <FiList /></div>
+                                        <div className="product-img">
+                                             <img src={product.image_url} alt={product.name} />
                                         </div>
-                                        <div className="discount">
-                                        <span className="off">{product.price_discount}</span>
-                                        <span className="old">R$ {product.old_price}</span>
+                                        <button
+                                            className="add-btn"
+                                            onClick={()=> setScreen('login')}
+                                        >
+                                            +
+                                        </button>
+                                        <div className="product-info">
+                                            <p className="name">{product.name}</p>
+        
+                                            <div className="price">
+                                            <span className="current">R$ {product.price}</span>
+                                            <span className="unit">/{product.unit_type}</span>
+                                            </div>
+                                            <div className="discount">
+                                            <span className="off">{product.price_discount}</span>
+                                            <span className="old">R$ {product.old_price}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                          )
-                        })}
-                    </Swiper>
-         </>
-     
-      )
+                </div>
+    </>
+)
 }
 
 export default ProductSwiper
