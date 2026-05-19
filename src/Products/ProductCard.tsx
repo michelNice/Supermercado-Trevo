d230a99ff791a260293aac2e965d6796b241602b
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-05-19 08:50:22 -0300

    Update

--- src/Products/ProductCard.tsx
+++ src/Products/ProductCard.tsx
@@ -3,16 +3,16 @@ import { FiList } from "react-icons/fi";
 type ProductSectionProps = {
         setScreen: React.Dispatch<React.SetStateAction<string>>
         setShowModal: React.Dispatch<React.SetStateAction<boolean>>
-        showModal: boolean
-        showUnavailable: React.Dispatch<React.SetStateAction<boolean>>
-        setShowUnavailable: React.Dispatch<React.SetStateAction<boolean>>
         product:productApi;
+         showDiscount?: boolean
+      
 }
 
-const ProductCard: React.FC<ProductSectionProps> = ({setShowModal,showModal,product,setScreen}) => {
+const ProductCard: React.FC<ProductSectionProps> = ({setShowModal,product,setScreen,showDiscount = true}) => {
 return (
     <>
          <div className="product-card">
+                                { showDiscount && (
                                     <div
                                         className={`offer ${
                                             product?.offer?.trim().toLowerCase() === 'exclusivo'
@@ -22,6 +22,7 @@ return (
                                         >
                                         {product?.offer}
                                     </div>
+                                    )}
                                         <div className="filter-icon" onClick={()=> setShowModal(true)}> <FiList /></div>
                                         <div className="product-img">
                                              <img src={product.image_url} alt={product.name} />
@@ -39,11 +40,14 @@ return (
                                             <span className="current">R$ {product.price}</span>
                                             <span className="unit">/{product.unit_type}</span>
                                             </div>
+                                            {showDiscount && (
                                             <div className="discount">
                                             <span className="off">{product.price_discount}</span>
                                             <span className="old">R$ {product.old_price}</span>
+                                            
                                             </div>
-                                        </div>
+                                        )} 
+                             </div>
                 </div>
     </>
 )
