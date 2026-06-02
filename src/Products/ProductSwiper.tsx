3a74f213491883995037b62c28d5080b1498f301
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-06-02 08:44:33 -0300

    update

---
+++ src/Products/ProductSwiper.tsx
@@ -0,0 +1,28 @@
+import { Swiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+type Props = {
+    children: React.ReactNode;
+}
+const ProductSwiper = ({ children }: Props) => {
+    return (
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={16}
+      slidesPerView={2.2}
+      navigation
+      grabCursor
+      breakpoints={{
+        640: { slidesPerView: 2.2 },
+        768: { slidesPerView: 3 },
+        1024: { slidesPerView: 5 },
+      }}
+      className="products-swiper"
+    >
+      {children}
+    </Swiper>
+  );
+}
+
+export default ProductSwiper;
---
+++ src/Types/Types.ts
@@ -0,0 +1,31 @@
+//Products e detaius
+export type productApi = {
+     price:  number
+     id:string 
+     description?: string
+     image_url :string 
+     name : string 
+     old_price:number
+     new_price:number
+     price_discount: string
+     offer?:string
+     category:string
+     unit_type:string
+}
+
+export type productSectetion = {
+  products: productApi[]
+  setProducts: React.Dispatch<React.SetStateAction<productApi[]>>
+  setScreen: React.Dispatch<React.SetStateAction<string>>
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+  showModal: boolean
+  setSelectedProduct: React.Dispatch<React.SetStateAction<productApi | null>>
+}
+
+//mobileNav
+export type MobileMenuItems = {
+  icon: React.ReactNode;
+  title: string;
+  desc: string;
+   link?: string
+}
