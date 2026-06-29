9d684d7decf5a26b12523a10b8004095ebc29ec1
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-06-28 21:54:15 -0300

    update

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,33 +1,87 @@
 import { useCart } from "../../context/useCart";
+import "./ShoppingCart.scss";
 
 export default function ShoppingCart() {
-  const { cartItem, cartTotal, removeFromCart ,decreaseQuantity} = useCart();
+  const {
+    cartItem,
+    cartTotal,
+    removeFromCart,
+    decreaseQuantity,
+    //increaseQuantity,
+  } = useCart();
 
   return (
-    <div>
-      <h2>Carrinho</h2>
+    <div className="shopping-cart">
+      <h2 className="shopping-cart__title">
+        Meu Carrinho
+      </h2>
 
-     {cartItem.map((item) => {
-  console.log(item);
+      {cartItem.length === 0 && (
+        <p className="shopping-cart__empty">
+          Seu carrinho está vazio.
+        </p>
+      )}
 
-  return (
-    <div key={item.id}>
-      <img src={item.image} alt={item.name} />
-      <p>{item.name}</p>
-      <p>{item.quantity}</p>
+      {cartItem.map((item) => (
+        <div
+          className="shopping-cart__item"
+          key={item.id}
+        >
+          <img
+            className="shopping-cart__image"
+            src={item.image}
+            alt={item.name}
+          />
 
-       <button onClick={() => decreaseQuantity(item.id)}>
-    -
-  </button>
+          <div className="shopping-cart__info">
+            <h3>{item.name}</h3>
 
-      <button onClick={() => removeFromCart(item.id)}>
-        Remover
-      </button>
-    </div>
-  );
-})}
+            <span className="shopping-cart__price">
+              R$ {item.price.toFixed(2)}
+            </span>
+          </div>
 
-      <div>Total: {cartTotal}</div>
+          <div className="shopping-cart__quantity">
+
+            <button
+              onClick={() => decreaseQuantity(item.id)}
+            >
+              -
+            </button>
+
+            <span>{item.quantity}</span>
+
+           
+
+          </div>
+
+          <div className="shopping-cart__subtotal">
+
+            R$
+            {(item.price * item.quantity).toFixed(2)}
+
+          </div>
+
+          <button
+            className="shopping-cart__remove"
+            onClick={() => removeFromCart(item.id)}
+          >
+            Remover
+          </button>
+        </div>
+      ))}
+
+      <div className="shopping-cart__footer">
+
+        <h2>
+          Total: R$ {cartTotal.toFixed(2)}
+        </h2>
+
+        <button className="shopping-cart__finish">
+          Finalizar Compra
+        </button>
+
+      </div>
     </div>
   );
 }
