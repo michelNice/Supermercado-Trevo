5e0c0eeda5e15d6505b4d68d5d8813c7047a2249
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-07-15 22:01:48 -0300

    update

--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -1,12 +1,36 @@
-import { Router } from "express"
-import type { Request, Response } from "express"
+import { Router } from "express";
+import type { Request, Response } from "express";
+import client from "../config/mercadoPago";
+import { Preference } from "mercadopago";
 
-const router = Router()
+const router = Router();
 
-router.post("/", async (_req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const preference = new Preference(client);
+
+    const response = await preference.create({
+      body: {
+        items: [
+          {
+            id: "1",
+            title: "Compra Trevo",
+            quantity: 1,
+            unit_price: 100,
+          },
+        ],
+      },
+    });
+
     res.json({
-        message: "Pagamento Mercado Pago funcionando"
-    })
-})
+      preferenceId: response.id,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Erro ao criar pagamento",
+    });
+  }
+});
 
-export default router
+export default router;
